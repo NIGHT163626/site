421eec6c99f76ff7ee39548cf29f48b5aac8581f
Ki Night <kinightrd@gmail.com>
2025-06-20 05:23:45 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Meu site RAW</title>
+  </head>
+  <body>
+    <h1>Bem-vindo</h1>
+    <p>Acesse <a href="/raw">/raw</a> para ver o script.</p>
+  </body>
+</html>
